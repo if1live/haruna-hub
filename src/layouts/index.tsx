c33db81f2608c7fc43a16476af23fed1b61997f4
if1live <libsora25@gmail.com>
2025-08-11 23:26:42 +0900

    favicon

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -13,6 +13,7 @@ const Layout: FC = (props) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Haruna Hub</title>
+        <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
           rel="stylesheet"
