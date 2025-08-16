a4022f384ec21315033c023e746575c678b0d33a
if1live <libsora25@gmail.com>
2025-08-16 22:53:45 +0900

    로컬 환경에서 정적파일 서빙

--- src/dev.ts
+++ src/dev.ts
@@ -1,5 +1,8 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { app } from "./app";
+
+app.use("*", serveStatic({ root: "./public" }));
 
 // cloudflare worker 개발환경과 맞춤
 const port = 8787;
